8703c6bf682bbd32f9a71c2717512bf223ecdb88
ryannwaneri <69826492+ryannwaneri@users.noreply.github.com>
2021-02-09 23:48:16 +0100

    Add files via upload

---
+++ python voter.py
@@ -0,0 +1,17 @@
+amount=int(input("please enter the amout of nominees:"))
+nominees={}
+choice=""
+
+for x in range(amount):
+    nomin=input("please enter nominee name:")
+    nominees[nomin]=0
+    
+while not choice=="-1":
+    choice=input("please enter your chosen candidate:")
+    if not choice=="-1" and  choice in nominees.keys():
+        nominees[choice]=nominees[choice]+1
+    else:
+        print("please enter a proper candidate")
+        
+for k,v in nominees.items():
+    print(k+" has a total vote of "+str(v))
